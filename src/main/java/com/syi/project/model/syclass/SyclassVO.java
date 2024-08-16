23a36f1c1ac1897fc7cee0e01eb9e600c976cbb4
kimdoyeon <lemon8818@naver.com>
2024-08-16 20:48:05 +0900

    [도연] #40

--- src/main/java/com/syi/project/model/syclass/SyclassVO.java
+++ src/main/java/com/syi/project/model/syclass/SyclassVO.java
@@ -32,9 +32,9 @@ public class SyclassVO {
     private Date modifiedDate;
     
     private String classStatus;
-    
-    private String isDeleted;
 	
     private int adminNo;
+    
+    private String isDeleted;
 
 }
