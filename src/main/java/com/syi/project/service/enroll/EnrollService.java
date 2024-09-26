c62ff7a369b9aa04b5b17399bc2c676d00b45e84
YJ-2 <dbwls7233@naver.com>
2024-09-26 15:54:23 +0900

    feat: oracle에서 mysql로 수정

--- src/main/java/com/syi/project/service/enroll/EnrollService.java
+++ src/main/java/com/syi/project/service/enroll/EnrollService.java
@@ -7,16 +7,16 @@ import com.syi.project.model.member.MemberVO;
 
 public interface EnrollService {
 
-	// 수강 중인 반 조회
-	List<EnrollVO> selectEnrollList(int memberNo);
+  // 수강 중인 반 조회
+  List<EnrollVO> selectEnrollList(int memberNo);
 
-	// 수강 중인 반에서 classNo 작은 값 조회
-	int selectClassNo(int memberNo);
+  // 수강 중인 반에서 classNo 작은 값 조회
+  int selectClassNo(int memberNo);
 
-	// 특정 클래스의 수강 중인 수강생 조회
-    List<EnrollVO> selectMemberList(int classNo);
+  // 특정 클래스의 수강 중인 수강생 조회
+  List<EnrollVO> selectMemberList(int classNo);
 
-	// 반 별 수강생 리스트 조회
-	List<MemberVO> selectStudentList(int classNo);
+  // 반 별 수강생 리스트 조회
+  List<MemberVO> selectStudentList(int classNo);
 
 }
